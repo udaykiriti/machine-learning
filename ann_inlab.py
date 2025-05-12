e3a23b0dc97f4421aa00c2eaeca6e00846dd6504
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-12 08:04:53 +0530

    Update ann_inlab.py

--- ann_inlab.py
+++ ann_inlab.py
@@ -22,7 +22,6 @@ y = iris_data['class']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train the Decision Tree Classifier
 clf = DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 
