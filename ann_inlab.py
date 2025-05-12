03d5bd4e82755dec79f287d7bb651da712be9851
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-12 08:05:05 +0530

    Update ann_inlab.py

--- ann_inlab.py
+++ ann_inlab.py
@@ -25,7 +25,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 clf = DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 
-# Make predictions
 y_pred = clf.predict(X_test)
 
 # Evaluate the model
