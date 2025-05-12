16d0ddff98ca2185ec04096cefd81b775f8142b0
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-12 08:03:56 +0530

    Update ann_inlab.py

--- ann_inlab.py
+++ ann_inlab.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import classification_report, confusion_matrix
 
-# Load dataset
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
 iris_data = pd.read_csv(url, header=None, names=columns)
